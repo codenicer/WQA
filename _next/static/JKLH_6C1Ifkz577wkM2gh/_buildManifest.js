d268c1405ba6ed0d9c4182c0a2462ff9fc7a3616
codenicer <ruthertenido@gmail.com>
2021-03-05 15:25:11 +0000

    Deploying to gh-pages from  @ 7fed849e98fc6298b41d1e19db35a5fec3634afa 🚀

---
+++ _next/static/JKLH_6C1Ifkz577wkM2gh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fce09832bb9aa49faaaaf.css","static\u002Fchunks\u002Fpages\u002Findex-7c59cdc8140b58ae9dbd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e6a8d84e13468fcb7816.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
