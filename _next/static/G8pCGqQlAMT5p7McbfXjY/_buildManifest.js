577a6d1c07f3fbef51d2ecab9c3af49b7f2d5374
codenicer <ruthertenido@gmail.com>
2021-03-05 16:05:55 +0000

    Deploying to gh-pages from  @ 9f0b461fe20f13e6ae5174f90707881b723e6a2a 🚀

---
+++ _next/static/G8pCGqQlAMT5p7McbfXjY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F400f79834c7960f31c56.css","static\u002Fchunks\u002Fpages\u002Findex-3752c892dc2a1766df90.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e6a8d84e13468fcb7816.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/zF3e5Lo1LxiIWz1NbjRW0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F0024e6329ae1b6c6b3af.css","static\u002Fchunks\u002Fpages\u002Findex-3592999a82d4c8c4c20c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e6a8d84e13468fcb7816.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
